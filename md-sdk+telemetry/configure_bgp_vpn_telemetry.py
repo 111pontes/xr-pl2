c3e62223671e1906ea633d88d7e1ff2cff548886
Santiago Alvarez <saalvare@cisco.com>
2018-06-12 21:51:46 -0700

    Add updated material for Cisco Live US 2018

---
+++ md-sdk+telemetry/configure_bgp_vpn_telemetry.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+#
+# Copyright 2018 Cisco Systems, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+Create configuration for BGP VPN telemetry.
+
+usage: configure_bgp_vpn_telemetry.py [-h] [-v] subscription_id ipv4_address destination_port device
+
+positional arguments:
+  subscription_id      telemetry subscription id
+  ipv4_address         collector IPv4 address
+  destination_port     collector destination port
+  device               NETCONF device (ssh://user:password@host:port)
+
+optional arguments:
+  -h, --help     show this help message and exit
+  -v, --verbose  print debugging messages
+"""
+
+import argparse
+import urllib.parse
+import sys
+
+from ydk.services import CRUDService
+from ydk.providers import NetconfServiceProvider
+from ydk.models.cisco_ios_xr import Cisco_IOS_XR_telemetry_model_driven_cfg \
+    as xr_telemetry_model_driven_cfg
+from ydk.types import Empty
+import logging
+
+
+def configure_bgp_vpn_telemetry(telemetry_model_driven, subscription_id, ipv4_address, destination_port):
+    """Add config data to telemetry object."""
+    # destination group
+    destination_group = telemetry_model_driven.destination_groups.DestinationGroup()
+    destination_group.destination_id = "PIPELINE"
+    ipv4_destination = destination_group.ipv4_destinations.Ipv4Destination()
+    ipv4_destination.destination_port = destination_port
+    ipv4_destination.ipv4_address = ipv4_address
+    ipv4_destination.encoding = xr_telemetry_model_driven_cfg.EncodeType.self_describing_gpb
+    protocol = ipv4_destination.Protocol()
+    protocol.protocol = xr_telemetry_model_driven_cfg.ProtoType.grpc
+    protocol.no_tls = 1
+    ipv4_destination.protocol = protocol
+    destination_group.ipv4_destinations.ipv4_destination.append(ipv4_destination)
+    telemetry_model_driven.destination_groups.destination_group.append(destination_group)
+
+    # sensor group
+    sensor_group = telemetry_model_driven.sensor_groups.SensorGroup()
+    sensor_group.sensor_group_identifier = "BGP-VPN-SENSORS"
+    sensor_group.enable = Empty()
+    sensor_path = sensor_group.sensor_paths.SensorPath()
+    sensor_path.telemetry_sensor_path = "Cisco-IOS-XR-telemetry-model-driven-oper:telemetry-model-driven/subscriptions/subscription" 
+    sensor_group.sensor_paths.sensor_path.append(sensor_path)
+    sensor_path = sensor_group.sensor_paths.SensorPath()
+    sensor_path.telemetry_sensor_path = "Cisco-IOS-XR-ipv4-bgp-oper:bgp/instances/instance/instance-active/default-vrf/neighbors/neighbor" 
+    sensor_group.sensor_paths.sensor_path.append(sensor_path)
+    sensor_path = sensor_group.sensor_paths.SensorPath()
+    sensor_path.telemetry_sensor_path = "Cisco-IOS-XR-ip-rib-ipv4-oper:rib/vrfs/vrf/afs/af/safs/saf/ip-rib-route-table-names/ip-rib-route-table-name/routes/route" 
+    sensor_group.sensor_paths.sensor_path.append(sensor_path)
+    telemetry_model_driven.sensor_groups.sensor_group.append(sensor_group)
+
+    # subscription
+    subscription = telemetry_model_driven.subscriptions.Subscription()
+    subscription.subscription_identifier = subscription_id
+    sensor_profile = subscription.sensor_profiles.SensorProfile()
+    sensor_profile.sensorgroupid = "BGP-VPN-SENSORS"
+    sensor_profile.sample_interval = 5000
+    subscription.sensor_profiles.sensor_profile.append(sensor_profile) 
+    destination_profile = subscription.destination_profiles.DestinationProfile()
+    destination_profile.destination_id = "PIPELINE"
+    destination_profile.enable = Empty()
+    subscription.destination_profiles.destination_profile.append(destination_profile) 
+    telemetry_model_driven.subscriptions.subscription.append(subscription) 
+
+
+if __name__ == "__main__":
+    """Execute main program."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", 
+                        help="print debugging messages",
+                        action="store_true")
+    parser.add_argument("subscription_id",
+                        help="telemetry subscription id")
+    parser.add_argument("ipv4_address",
+                        help="collector IPv4 address")
+    parser.add_argument("destination_port",
+                        help="collector destination port")
+    parser.add_argument("device",
+                        help="NETCONF device (ssh://user:password@host:port)")
+    args = parser.parse_args()
+    device = urllib.parse.urlparse(args.device)
+
+    # log debug messages if verbose argument specified
+    if args.verbose:
+        logger = logging.getLogger("ydk")
+        logger.setLevel(logging.INFO)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(("%(asctime)s - %(name)s - "
+                                      "%(levelname)s - %(message)s"))
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    # create NETCONF provider
+    provider = NetconfServiceProvider(address=device.hostname,
+                                      port=device.port,
+                                      username=device.username,
+                                      password=device.password,
+                                      protocol=device.scheme)
+    # create CRUD service
+    crud = CRUDService()
+
+    # telemetry configuration
+    telemetry_model_driven  = xr_telemetry_model_driven_cfg.TelemetryModelDriven()
+    configure_bgp_vpn_telemetry(telemetry_model_driven,
+                                args.subscription_id,
+                                args.ipv4_address,
+                                args.destination_port)
+
+    # create configuration on NETCONF device
+    crud.create(provider, telemetry_model_driven)
+
+    sys.exit()
+# End of script
---
+++ md-sdk+telemetry/verify_bgp_neighbor.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+#
+# Copyright 2018 Cisco Systems, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Verify BGP neighbor operation.
+
+usage: verify_bgp_neighbor.py [-h] [-v] node neighbor_address
+
+positional arguments:
+  node               node streaming interface status
+  neighbor_address   neighbor address
+
+optional arguments:
+  -h, --help     show this help message and exit
+  -v, --verbose  print debugging messages
+"""
+
+import kafka
+import sys
+import json
+import time
+import logging
+
+from argparse import ArgumentParser
+
+KAFKA_TOPIC = 'pipeline'
+KAFKA_BOOTSTRAP_SERVER = 'localhost:9092'
+SESSION_STATE_ESTABLISHED = "bgp-st-estab"
+VALIDATION_TIMEOUT = 30
+
+
+def verify_bgp_neighbor(kafka_consumer, node, neighbor_address,
+                        session_state=SESSION_STATE_ESTABLISHED,
+                        timeout=VALIDATION_TIMEOUT):
+    """Verify BGP session state."""
+    telemetry_encoding_path = "Cisco-IOS-XR-ipv4-bgp-oper:bgp/instances/instance/instance-active/default-vrf/neighbors/neighbor"
+    start_time = time.time()
+    # iterate over all arriving messages
+    for kafka_msg in kafka_consumer:
+        msg = json.loads(kafka_msg.value.decode('utf-8'))
+        # if message is operational BGP data
+        if (msg["Telemetry"]["node_id_str"] == node and
+                msg["Telemetry"]["encoding_path"] == telemetry_encoding_path
+                and "Rows" in msg):
+            for row in msg["Rows"]:
+                # if neighbor in intended session state
+                if (row["Keys"]["instance-name"] == "default"
+                        and row["Keys"]["neighbor-address"] == neighbor_address
+                        and row["Content"]["connection-state"] == session_state):
+                    return True
+
+        if time.time() - start_time > timeout:
+            break
+
+    return False
+
+
+if __name__ == "__main__":
+    """Execute main program."""
+    parser = ArgumentParser()
+    parser.add_argument("-v", "--verbose", help="print debugging messages",
+                        action="store_true")
+    parser.add_argument("node",
+                        help="node router streaming interface status")
+    parser.add_argument("neighbor_address",
+                        help="neighbor address")
+    args = parser.parse_args()
+
+    # log debug messages if verbose argument specified
+    if args.verbose:
+        logger = logging.getLogger("kafka")
+        logger.setLevel(logging.INFO)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(("%(asctime)s - %(name)s - "
+                                      "%(levelname)s - %(message)s"))
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    # create kafka consumer to pipeline topic
+    kafka_consumer = kafka.KafkaConsumer(KAFKA_TOPIC,
+                                         bootstrap_servers=KAFKA_BOOTSTRAP_SERVER,
+                                         consumer_timeout_ms=VALIDATION_TIMEOUT*1000)
+
+    print(verify_bgp_neighbor(kafka_consumer, 
+                            args.node, args.neighbor_address))
+
+    sys.exit()
+# End of script
